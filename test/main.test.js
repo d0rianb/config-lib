d892bbfa89a35f9b927a28344cebfdf67d642636
d0rianb <dorian.beauchesne@icloud.com>
2020-09-06 16:03:03 +0200

    Fix read issue

--- test/main.test.js
+++ test/main.test.js
@@ -1,9 +1,16 @@
 const Config = require('../lib/main')
 
 const clientConf = new Config('client')
+const keyBindConf = new Config('keybind')
+
+clientConf.set('keybind', true)
+
+keyBindConf.set({
+    move: 'A',
+    shoot: 'B'
+})
 
 clientConf.set('name', 'Dorian')
-clientConf.set('age', 19)
-clientConf.save()
 
-console.log(clientConf.get())
+clientConf.save()
+keyBindConf.save()
